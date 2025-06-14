94e3fa6b1e9867bc7c065a8162979a4c388f4324
Dominik <domijas96@gmail.com>
2025-06-14 15:13:19 +0200

    [Feat] Updates

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,17 +1,18 @@
 import {useEffect, useState} from "react";
-import axios from "axios";
+import axios from "../utils/axios";
 
 export default function Home() {
     const [message, setMessage] = useState('');
 
     useEffect(() => {
         axios
-            .get(import.meta.env.VITE_API_URL)
+            .get('/')
             .then(response => {
                 setMessage(response.data.message);
             })
             .catch(error => {
                 console.error('Error fetching data:', error);
+                setMessage('Error fetching data');
             });
 
     }, [])
